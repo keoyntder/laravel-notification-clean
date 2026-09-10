7673b407a4d5e4c3c8ce6176d595b0548efe9971
Kent <kentmiken@gmail.com>
2026-09-10 22:21:05 +0800

    Add homepage redirect

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [RegisterController::class, 'showForm'])->name('register.form');
-Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
+Route::get('/', function () {
+    return redirect()->route('register.form');
+});
+
+Route::get('/register', [RegisterController::class, 'showForm'])
+    ->name('register.form');
+
+Route::post('/register', [RegisterController::class, 'register'])
+    ->name('register.submit');
